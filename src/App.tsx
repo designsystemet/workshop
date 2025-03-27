84d05816d96e39a3a75e47d5009b1c78ae153d4c
Michael Marszalek <mimarz@gmail.com>
2025-03-27 10:38:44 +0100

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import './App.css'
 
 function App() {
   return (
-    <>
+    <div>
       <Heading data-size='lg' level={1}>Velkommen til Designsystemet workshop.</Heading>
       <Paragraph>Redigere kildekoden på denne siden iht. designet ditt</Paragraph>
-    </>
+    </div>
   )
 }
 
